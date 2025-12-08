cd3678087e30da8a0867737f0e6aab7e08239ade
Yusuf <ahmed.yusuf.royal@gmail.com>
2025-12-08 19:36:36 +0530

    Fix Page 2

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,4 +2,4 @@ import streamlit as st
 st.title("Page 2")
 st.write("This is the second page.")
 
-st.page_link("home.py", label="Back to Home", icon="🏠")
+st.page_link("Home.py", label="Back to Home", icon="🏠")
