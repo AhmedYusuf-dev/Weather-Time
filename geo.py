4d65df4b903617b13227d7dc9d350b36c29436b3
Yusuf <ahmed.yusuf.royal@gmail.com>
2026-01-04 14:25:52 +0530

    1.2.2.3

--- geo.py
+++
@@ -1,3 +0,0 @@
-import geopandas as gpd
-gdf = gpd.read_file("ne_10m_populated_places.shp")
-gdf[['NAME','ADM0NAME','LATITUDE','LONGITUDE','POP_MAX']].to_csv("natural_earth_cities.csv", index=False)
